194e0693de1080e48c60a0d02d890a38f5b9a094
Mehran1470 <mhranzary40@gmail.com>
2026-09-05 09:04:08 +0330

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// این فایل رو با اطلاعات پروژه‌ی Firebase خودت پر کن.
+// راهنما توی README.md هست: چطور یه پروژه‌ی Firebase رایگان بسازی
+// و Realtime Database فعال کنی.
+
+export const firebaseConfig = {
+  apiKey: "PUT_YOUR_API_KEY_HERE",
+  authDomain: "PUT_YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://PUT_YOUR_PROJECT-default-rtdb.firebaseio.com",
+  projectId: "PUT_YOUR_PROJECT",
+  storageBucket: "PUT_YOUR_PROJECT.appspot.com",
+  messagingSenderId: "000000000000",
+  appId: "PUT_YOUR_APP_ID"
+};
