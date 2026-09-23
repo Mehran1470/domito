0ceba56b6acbac03c815c62bfaae8b5d31f55365
Mehran1470 <mhranzary40@gmail.com>
2026-09-23 12:58:42 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1124,13 +1124,7 @@ export const GAMES = [
     soloThreshold: 1
   },
 
-  {
-    id: "lightpuzzle",
-    name: "پازل نوری",
-    desc: "آینه، لنز و منشور را بچین و نور را به هدف‌ها برسان",
-    icon: "💡",
-    soloThreshold: 1
-  }
+ 
 ];
 export function soloWon(
   gameId,
